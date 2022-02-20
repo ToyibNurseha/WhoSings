4a09d15b840c0a63f7857990ba7d7f0776f92658
toyib nurseha <toyibseha@gmail.com>
2022-02-21 05:50:25 +0700

    adding App center crashlytics

--- app/src/main/java/com/toyibnurseha/whosings/MainActivity.kt
+++ app/src/main/java/com/toyibnurseha/whosings/MainActivity.kt
@@ -2,8 +2,12 @@ package com.toyibnurseha.whosings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import com.toyibnurseha.whosings.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,5 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCenter.start(
+            application, "c61ee56d-d41b-446d-8e59-bf618bd595fe",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
